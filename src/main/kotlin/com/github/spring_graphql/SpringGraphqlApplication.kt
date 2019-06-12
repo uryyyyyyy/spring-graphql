bc4fd346fecb5b83f2657b935036a5e1a346cda2
kokishibata <koki@anymindgroup.com>
2019-06-12 17:37:57 +0700

    init

---
+++ src/main/kotlin/com/github/spring_graphql/SpringGraphqlApplication.kt
@@ -0,0 +1,11 @@
+package com.github.spring_graphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringGraphqlApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringGraphqlApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/spring_graphql/SpringGraphqlApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.github.spring_graphql
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringGraphqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
